2504e7ba6ea9072e316807f59f4d9ec38755a748
Jon Doane <jdoane@vlacs.org>
2014-07-23 06:55:51 -0400

    Ready for usage elsewhere. 0.1.0 ready.

--- README.md
+++ README.md
@@ -1,48 +1,35 @@
-# stateful
-
-TODO: Brief description
-
-
+# Stateful
+State is one of those things where despite as much as you want to make your
+library and every app you write to be purely functional, sometimes you need
+state.Stateful is a library that a developer to use system state in a consistent
+way in both practical and easy ways. It is a simple library that deps on nothing
+but Clojure 1.6.
 
 ## Releases and Dependency Information
 
-* Releases are published to TODO_LINK
-
-* Latest stable release is TODO_LINK
-
-* All released versions TODO_LINK
-
 [Leiningen] dependency information:
 
-    [stateful "0.1.0-SNAPSHOT"]
+    [org.vlacs/stateful "0.1.0"]
 
 [Maven] dependency information:
 
     <dependency>
       <groupId>stateful</groupId>
       <artifactId>stateful</artifactId>
-      <version>0.1.0-SNAPSHOT</version>
+      <version>0.1.0</version>
     </dependency>
 
 [Leiningen]: http://leiningen.org/
 [Maven]: http://maven.apache.org/
 
-
-
 ## Usage
 
-TODO
-
+TODO: Coming soon!
 
 
 ## Change Log
 
-* Version 0.1.0-SNAPSHOT
+* Version 0.1.0
+  * Initial release
 
 
-
-## Copyright and License
-
-Copyright © 2014 TODO_INSERT_NAME
-
-TODO: [Choose a license](http://choosealicense.com/)
--- src/stateful.clj
+++ src/stateful.clj
@@ -1,3 +1,69 @@
-(ns stateful)
+(ns stateful
+  (:require [clojure.edn]))
 
+(def system-object (atom nil))
 
+(defn get-system-object
+  ([]
+   (get-system-object system-object))
+  ([mutable]
+   (identity mutable)))
+
+(defn set-state!
+  ([new-value]
+   (set-state! (get-system-object) new-value))
+  ([mutable new-value]
+   (reset! mutable new-value)))
+
+(defn get-state
+  ([]
+   (get-state (get-system-object)))
+  ([mutable]
+   @mutable))
+
+(defn put-in!
+  ([path item]
+   (put-in! (get-system-object) path item))
+  ([mutable path item]
+   (swap! mutable assoc-in path item)))
+
+(defn get-from
+  ([path]
+   (get-from (get-system-object) path))
+  ([mutable path]
+   (get-in (get-state mutable) path)))
+
+(defn transition!
+  ([transition-fn]
+   (transition! (get-system-object) transition-fn))
+  ([mutable transition-fn]
+   (set-state! mutable (transition-fn (get-state mutable)))))
+
+(defn load-config!
+  ([config-file-path]
+   (load-config! [:config] config-file-path))
+  ([path config-file-path]
+   (load-config! (get-system-object) path config-file-path))
+  ([mutable path config-file-path]
+   (when-let [edn-str (slurp config-file-path)]
+     (put-in!
+       mutable path
+       (clojure.edn/read-string edn-str)))))
+
+(defn init!
+  ([]
+   (init! {}))
+  ([initial-state]
+   (init! (get-system-object) initial-state))
+  ([mutable initial-state]
+   (set-state! mutable initial-state)
+   mutable))
+
+(def start! transition!)
+(def stop! transition!)
+
+(defn destroy!
+  ([]
+   (destroy! (get-system-object)))
+  ([mutable]
+   (set-state! mutable nil)))
